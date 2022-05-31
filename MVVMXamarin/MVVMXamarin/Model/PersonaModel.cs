f8b56f80cd1e9e3e050b2ec4182d9a4d43ce6c5e
unknown <facundoromani90@gmail.com>
2022-05-31 16:37:08 -0300

    CRUD Xamarin Forms

---
+++ MVVMXamarin/MVVMXamarin/Model/PersonaModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MVVMXamarin.Model
+{
+    public class PersonaModel:INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        private bool isBusy=false;
+
+        public bool Isbusy
+        {
+            get { return isBusy=false; }
+            set { isBusy=value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        // PropFull. 
+        private string id;
+        private string nombre;
+        private string apellido;
+        private int edad;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(nombreCompleto));
+            }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(nombreCompleto));
+            }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // PropFull para concatenar dos variables y luego hacer el Binding.
+
+        private string nombreCompleto;
+
+        public string NombreCompleto
+        {
+            get { 
+                return $"{Nombre} {Apellido}"; }  // Concatenación.
+            set { nombreCompleto = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(mensaje));
+            }
+        }
+
+        private string mensaje;
+
+        public string Mensaje
+        {
+            get { return $"Tu nombre es {nombreCompleto}"; }
+        }
+
+    }
+}
---
+++ MVVMXamarin/MVVMXamarin/ViewModel/PersonaViewModel.cs
@@ -0,0 +1,90 @@
+﻿using MVVMXamarin.Model;
+using MVVMXamarin.Servicio;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVMXamarin.ViewModel
+{
+    public class PersonaViewModel : PersonaModel
+    {
+        public ObservableCollection<PersonaModel> Personas { get; set; }
+        PersonaServicio servicio = new PersonaServicio();
+        PersonaModel modelo;
+
+        // Constructor.
+        public PersonaViewModel()
+        {
+            Personas = servicio.Consultar();
+            GuardarCommand = new Command(async () => await Guardar(), () => !Isbusy);
+            ModificarCommand = new Command(async () => await Modificar(), () => !Isbusy);
+            EliminarCommand = new Command(async () => await Eliminar(), () => !Isbusy);
+            LimpiarCommand = new Command(Limpiar, () => !Isbusy);
+        }
+
+        // Comandos.
+        public Command GuardarCommand { get; set; }
+        public Command ModificarCommand { get; set; }
+        public Command EliminarCommand { get; set; }
+        public Command LimpiarCommand { get; set; }
+
+        // Método que ejecuta el Command.
+        private async Task Guardar()
+        {
+            Isbusy = true;
+            Guid IdPersona = Guid.NewGuid(); // Generar un Id a persona.
+            modelo = new PersonaModel()
+            {
+                Nombre = Nombre,
+                Apellido = Apellido,
+                Edad = Edad,
+                Id = IdPersona.ToString()
+            };
+            servicio.Guardar(modelo);
+            Isbusy = false;
+        }
+
+        private async Task Modificar()
+        {
+            Isbusy = true;
+            modelo = new PersonaModel()
+            {
+                Nombre = Nombre,
+                Apellido = Apellido,
+                Edad = Edad,
+                Id = Id
+            };
+            servicio.Modificar(modelo);
+            await Task.Delay(2000);
+            Isbusy = false;
+        }
+
+        private async Task Eliminar()
+        {
+            Isbusy = true;
+            modelo = new PersonaModel()
+            {
+                Nombre = Nombre,
+                Apellido = Apellido,
+                Edad = Edad,
+                Id = Id
+            };
+            servicio.Eliminar(Id);
+            await Task.Delay(2000);
+            Isbusy = false;
+        }
+
+
+        // Método para limpiar campos.
+        private void Limpiar()
+        {
+            Nombre = "";
+            Apellido = "";
+            Edad = 0;
+            Id = "";
+        }
+    }
+}
